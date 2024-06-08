9f25fb3f8554cc82de2a36213a95de1b920ff468
maske <maskedwolf211@gmail.com>
2024-06-08 15:33:36 +0500

    i change this

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,40 @@
 // import { CardComponents } from "/src/Cards/CardComponents";
+import video from '../assets/videos/2.mp4'
 
 export default function Hero() {
   return (
     <>
-    <div>
-      <div className=" text-6xl font-bold mt-32 ml-4   font-serif">
-        Travel and feel <br />
-        your favorite <br />
-        place
-      </div>
-      <div>
-        <p className="ml-4 mt-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ab ipsa{" "}
-          <br />
-          quo, blanditiis quam cupiditate dolorum sequi tenetur, magni vel rem{" "}
-          <br />
-          aperiam quia velit libero corporis molestiae vero accusantium iste ad{" "}
-          <br />
-          dolorem?
-        </p>
-      </div>
-      <div className="mt-6">
+      <div className='flex'>
+        <div>
+          <div className=" text-6xl font-bold mt-32 ml-4   font-serif">
+            Travel and feel <br />
+            your favorite <br />
+            place
+          </div>
 
-      <a href="https://booking.com" target="_blank" className="px-4 py-1  font-semibold rounded-md bg-orange-500 hover:shadow-lg active:bg-orange-400  ml-4">
-        Explore
-      </a>
-      </div>
+          <p className="ml-4 mt-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ab
+            ipsa <br />
+            quo, blanditiis quam cupiditate dolorum sequi tenetur, magni vel rem{" "}
+            <br />
+            aperiam quia velit libero corporis molestiae vero accusantium iste
+            ad <br />
+            dolorem?
+          </p>
+        </div>
+        <div className="mt-6">
+          <a
+            href="https://booking.com"
+            target="_blank"
+            className="px-4 py-1  font-semibold rounded-md bg-orange-500 hover:shadow-lg active:bg-orange-400  ml-4"
+          >
+            Explore
+          </a>
+        </div>
 
-      <div>
-  
-      </div>
-      <video src=""></video>
-      
+        <div>
+          <video src="2.mp4">{video}</video>
+        </div>
       </div>
     </>
   );
